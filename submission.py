18138fb6c89365aa1be58afea5d1c0673a599131
olerigha <ol.erigha@gmail.com>
2026-09-24 23:53:45 -0500

    Finished Python Submission

--- submission.py
+++ submission.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 # F1
 # takes as an input a string of space separated integers that is split and converted to a list
 def multiply_list(numbers):
@@ -18,7 +21,7 @@ def multiply_list(numbers):
     return result   
 
 # Test cases for F1
-
+# print(multiply_list("1 2 3 4"))
     
 
 # F2
@@ -36,7 +39,7 @@ def count_common_chars(string):
     return len(unq1 & unq2)
 
 # Test cases for F2
-
+# print(count_common_chars("green red"))
 
 # F3
 # finds the sum of the numbers from 1 to N (inclusive) that are divisible by K
@@ -61,7 +64,7 @@ def sum_divisible_by_k(N,K):
 
 # Test Cases for F3
 
-print(sum_divisible_by_k(5,2))
+# print(sum_divisible_by_k(5,2))
 
 # F4
 # find the highest common factor of the 2 numbers
@@ -77,4 +80,58 @@ def highest_common_factor(a,b):
         if a % n == 0 and b % n == 0:
             return n
 
-print( highest_common_factor(8,6))
+# Test Cases for F4
+# print( highest_common_factor(8,6))
+
+
+# F5
+# return minimum of the list data
+
+# def get_minimum(list):
+#     return min(list)
+
+def get_minimum(list):
+
+    if not list:
+        return None
+    
+    small = list[0]
+
+    for n in list:
+        if n < small:
+            small = n
+    
+    return small
+
+
+# Test Cases for F5
+# print(get_minimum([3, 1, 4, 1, 5, 9, 2, 6]))
+
+# F6
+# accepts three arguments, a dataframe, old column name and the new column name. In the function, update the column name from old to the new and return the modified dataframe
+
+def rename_col(df,old_name,new_name):
+    df = df.rename(columns={old_name: new_name})
+    return df
+
+# F7
+# accepts a series and returns the standard deviation of that series
+
+def standard_deviation(series):
+    return series.std()
+
+# F8
+
+def correlation_sum(a):
+    numbers = [int(x) for x in a.split()]
+
+    if len(numbers) != 9:
+        return 0
+
+    df = pd.DataFrame(np.array(numbers).reshape(3, 3))
+
+    return round(df.corr().values.sum(), 1)
+
+print(correlation_sum("1 2 3 4 5 6 7 8 9"))
+print(correlation_sum("1 2 3 4 5 6"))
+print(correlation_sum("1 2 3 45 5 6 7 10 9"))
